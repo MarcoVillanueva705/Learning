e5129adb03f3ae8c73a5ce8f8f1457530eb4b2ce
marco villanueva <villanuevamarco98@gmail.com>
2020-07-12 17:08:51 -0600

    Add project files.

---
+++ Learning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Computing Data...");
+            Download();
+            Console.ReadLine();
+        }
+
+        //using an built-in async function from .NET library
+        //
+        static async void Download()
+        {
+            HttpClient client = new HttpClient();
+            var data = await client.GetStringAsync("http://rouxacademy.com");
+            Console.WriteLine("Data assimilated..." + data);
+        }
+    }
+}
