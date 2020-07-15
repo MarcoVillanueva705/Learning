099b4d8c3d49e84d6432aff642bb7129acec23f4
marco villanueva <villanuevamarco98@gmail.com>
2020-07-14 21:04:13 -0600

    static download methods

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -19,13 +19,35 @@ namespace Learning
 
         //using an built-in async function from .NET library
         
-        static async void Download()
-        {
-            HttpClient client = new HttpClient();
-            var data = await client.GetStringAsync("http://rouxacademy.com");
-            Console.WriteLine("Data assimilated..." + data);
-        }
+        //static async void Download()
+        //{
+        //    HttpClient client = new HttpClient();
+        //    var data = await client.GetStringAsync("http://rouxacademy.com");
+        //    Console.WriteLine("Data assimilated..." + data);
+        //}
         //for next segment anonymous fuctions, callbacks and action to manage
         //program flow
+
+        //Imaginary external network library
+
+         static void Download()
+        {
+            Network.Download(OnDownloaded);
+        }
+
+        static void OnDownloaded()
+        {
+            Console.WriteLine("Data assimilated...");
+        }
+
+        class Network
+        {
+            static public void Download(Action callback)
+            {
+                Task.Run(() => {
+                Thread.Sleep(3000);
+                    callback();
+            });
+        }
     }
 }
